60dc34acdb914aa2689af9d15c539dd771fac70e
gabriel ninow <gabrielninow@gmail.com>
2024-02-04 18:18:10 -0300

    added user signup feature

--- src/main/java/com/inn/cafe/constants/CafeConstants.java
+++ src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -1,4 +1,7 @@
 package com.inn.cafe.constants;
 
 public class CafeConstants {
+
+    public  static final String ERROR_MESSAGE = "Something went wrong";
+    public static final String INVALID_DATA = "Invalid data";
 }
